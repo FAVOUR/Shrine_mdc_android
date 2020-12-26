528c1bbc999d12c0f73bd6de3337e6c8b91283cb
FAVOUR <truelyfavoured@gmail.com>
2020-12-26 23:46:42 +0100

    Refactored StaggeredProductCardViewHolder.kt file

--- app/src/main/java/com/example/shrine_mdc_android/staggeredgridlayout/StaggeredProductCardViewHolder.kt
+++ app/src/main/java/com/example/shrine_mdc_android/staggeredgridlayout/StaggeredProductCardViewHolder.kt
@@ -1,13 +1,15 @@
 package com.example.shrine_mdc_android.staggeredgridlayout
 
 import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.toolbox.NetworkImageView
 import com.example.shrine_mdc_android.R
 
-class StaggeredProductCardViewHolder(itemView: View) {
+class StaggeredProductCardViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
 
     var produceImage: NetworkImageView =itemView.findViewById(R.id.product_image)
-    var productTitle: NetworkImageView =itemView.findViewById(R.id.product_title)
-    var productPrice: NetworkImageView =itemView.findViewById(R.id.product_price)
+    var productTitle: TextView =itemView.findViewById(R.id.product_title)
+    var productPrice: TextView =itemView.findViewById(R.id.product_price)
 
 }
