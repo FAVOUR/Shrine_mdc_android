d69079ded3c09090c9ca9b4aa288a4de35b2f346
FAVOUR <truelyfavoured@gmail.com>
2020-12-27 18:31:33 +0100

    Refactored the LoginFragment.kt file

--- app/src/main/java/com/example/shrine_mdc_android/LoginFragment.kt
+++ app/src/main/java/com/example/shrine_mdc_android/LoginFragment.kt
@@ -1,4 +1,49 @@
 package com.example.shrine_mdc_android
 
-class LoginFragment {
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.codelabs.mdc.kotlin.shrine.NavigationHost
+import com.google.codelabs.mdc.kotlin.shrine.ProductGridFragment
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate( R.layout.fragment_login,container,false)
+
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                password_text_input.error = null // Clear the error
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false) // Navigate to the next Fragment
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text)) {
+                password_text_input.error = null //Clear the error
+            }
+            false
+        }
+        return view
+    }
+
+
+    /*
+        In reality, this will have more complex logic including, but not limited to, actual
+        authentication of the username and password.
+     */
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
+
 }
