132a5d599fa1fa3f19944a3f9477e1fe3f8a2863
FAVOUR <truelyfavoured@gmail.com>
2020-12-27 18:31:52 +0100

    Refactored the MainActivity.kt file

--- app/src/main/java/com/example/shrine_mdc_android/MainActivity.kt
+++ app/src/main/java/com/example/shrine_mdc_android/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.shrine_mdc_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.codelabs.mdc.kotlin.shrine.NavigationHost
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, LoginFragment())
+                    .commit()
+        }
+    }
 
+    /**
+     * Navigate to the given fragment.
+     *
+     * @param fragment       Fragment to navigate to.
+     * @param addToBackstack Whether or not the current fragment should be added to the backstack.
+     */
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
