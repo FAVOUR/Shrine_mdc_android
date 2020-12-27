3c78c7cdd699aa276848ffc05910bcc3c2b7640f
FAVOUR <truelyfavoured@gmail.com>
2020-12-27 18:34:22 +0100

    Added the ProductGridFragment.kt file.

---
+++ app/src/main/java/com/example/shrine_mdc_android/ProductGridFragment.kt
@@ -0,0 +1,17 @@
+package com.google.codelabs.mdc.kotlin.shrine
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shrine_mdc_android.R
+
+class ProductGridFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+    }
+}
