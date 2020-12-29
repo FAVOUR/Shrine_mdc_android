54258ece91fa9b01140fe8377f1d907136a0a6ca
FAVOUR <truelyfavoured@gmail.com>
2020-12-29 21:21:42 +0100

    Updated the ProductGridFragment.kt class.

--- app/src/main/java/com/example/shrine_mdc_android/ProductGridFragment.kt
+++ app/src/main/java/com/example/shrine_mdc_android/ProductGridFragment.kt
@@ -1,17 +1,24 @@
-package com.google.codelabs.mdc.kotlin.shrine
+package com.example.shrine_mdc_android
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.shrine_mdc_android.R
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.*
 
 class ProductGridFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        // Inflate the layout for this fragment with the ProductGrid theme
+        val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+
+        // Set up the toolbar.
+        (activity as AppCompatActivity).setSupportActionBar(app_bar)
+
+        return view;
     }
 }
