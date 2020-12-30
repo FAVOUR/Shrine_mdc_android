370782e8e442c817230a4375ee5a54f95061e4e2
FAVOUR <truelyfavoured@gmail.com>
2020-12-30 18:36:54 +0100

    Passed the dependency to the parameter of the itemDecorator method the ProductGridItemDecoration.kt file

--- app/src/main/java/com/example/shrine_mdc_android/ProductGridItemDecoration.kt
+++ app/src/main/java/com/example/shrine_mdc_android/ProductGridItemDecoration.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
  * Custom item decoration for a vertical [ProductGridFragment] [RecyclerView]. Adds a
  * small amount of padding to the left of grid items, and a large amount of padding to the right.
  */
-class ProductGridItemDecoration(private val largePadding: Int, private val smallPadding: Int) : RecyclerView.ItemDecoration() {
+class ProductGridItemDecoration (private val largePadding: Int, private val smallPadding: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View,
                                 parent: RecyclerView, state: RecyclerView.State) {
